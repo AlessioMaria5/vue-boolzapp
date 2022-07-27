a0e285b076f50ffece45b940b72bf36ec12fda3c
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-27 17:32:17 +0200

    generazione contatti

--- js/main.js
+++ js/main.js
@@ -1,4 +1,185 @@
-const app = new Vue({
+var inizio = new Vue({
 
-    el: '#chatApp'
+    el: "#app",
+
+    data: {
+
+        contacts: [
+            
+            {
+                avatar: 'img/avatar_1.jpg',
+                name: "Michele",
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+
+                    {
+        
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    }
+                ],
+            },
+
+            {
+                avatar: 'img/avatar_2.jpg',
+                name: "Giovanni",
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+
+                    {
+        
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    }
+                ],
+            },
+
+            {
+                avatar: 'img/avatar_3.jpg',
+                name: "Alfredo",
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+
+                    {
+        
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    }
+                ],
+            },
+
+            {
+                avatar: 'img/avatar_4.jpg',
+                name: "Giovanni CEO",
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+
+                    {
+        
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    }
+                ],
+            },
+
+            {
+                avatar: 'img/avatar_5.jpg',
+                name: "Sara",
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+
+                    {
+        
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    }
+                ],
+            },
+
+
+            {
+                avatar: 'img/avatar_6.jpg',
+                name: "Ramona",
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+
+                    {
+        
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    }
+                ],
+            },
+
+
+            {
+                avatar: 'img/avatar_7.jpg',
+                name: "Giulio",
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+
+                    {
+        
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    }
+                ],
+            },
+
+
+            {
+                avatar: 'img/avatar_8.jpg',
+                name: "Carlo",
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+
+                    {
+        
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    }
+                ],
+            },
+
+
+
+
+
+
+            
+        ],
+
+    },
+
+    created: function(){
+        console.log(this.contacts)
+    }
 })
